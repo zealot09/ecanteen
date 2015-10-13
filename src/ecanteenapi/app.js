5376166758f30acfd3f7218cbf334577d6cf6b2f
zealot09 <guol.zealot@gmail.com>
2015-10-13 23:27:51 +0800

    add auth & login

--- src/ecanteenapi/app.js
+++ src/ecanteenapi/app.js
@@ -5,25 +5,29 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.all('/*', function(req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  // Set custom headers for CORS
+  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+  if (req.method == 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
+
+app.all('/api/v1/*', [require('./middlewares/validateRequest')]);
+app.use('/', require('./routes'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -31,30 +35,5 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
 
 module.exports = app;
--- src/ecanteenapi/routes/index.js
+++ src/ecanteenapi/routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var auth = require('./auth.js');
+var products = require('./products.js');
+var user = require('./users.js');
+
+console.log(auth);
+router.post('/login', auth.login);
+
+/*
+ * Routes that can be accessed only by autheticated users
+ */
+router.get('/api/v1/products', products.getAll);
+router.get('/api/v1/product/:id', products.getOne);
+router.post('/api/v1/product/', products.create);
+router.put('/api/v1/product/:id', products.update);
+router.delete('/api/v1/product/:id', products.delete);
+
+/*
+ * Routes that can be accessed only by authenticated & authorized users
+ */
+router.get('/api/v1/admin/users', user.getAll);
+router.get('/api/v1/admin/user/:id', user.getOne);
+router.post('/api/v1/admin/user/', user.create);
+router.put('/api/v1/admin/user/:id', user.update);
+router.delete('/api/v1/admin/user/:id', user.delete);
 
 module.exports = router;
